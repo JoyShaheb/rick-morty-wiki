1b1de576362217fedd00cf1d8d597debcf7e7671
joyshaheb <khondokoralam@gmail.com>
2023-01-01 18:49:57 +0600

    refactored some code

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
-import Characters from "./pages/Characters";
-import Error404 from "./pages/Error404";
-import Episode from "./pages/Episode";
-import Location from "./pages/Location";
+import {
+  CharacterDetails,
+  Location,
+  Episode,
+  Error404,
+  Characters,
+} from "./pages";
 
 function App() {
   const uiTheme = useSelector((state: any) => state.uiSettings.theme);
@@ -20,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Characters />} />
           <Route path="/characters" element={<Characters />} />
+          <Route path="/characters/:id" element={<CharacterDetails />} />
           <Route path="/Episode" element={<Episode />} />
           <Route path="/Location" element={<Location />} />
           <Route path="*" element={<Error404 />} />
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Characters from "./Characters";
+import CharacterDetails from "./CharacterDetails";
+import Episode from "./Episode";
+import Error404 from "./Error404";
+import Location from "./Location";
+
+export { Characters, CharacterDetails, Episode, Error404, Location };
